981009883d4cd198b567bcc952ed2784db77f84b
Miguel Pimenta <jmiguelpimenta@gmail.com>
2020-11-17 16:33:06 -0300

    implemented delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,23 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit
+    @task = Task.find(params[:id])
+    # raise
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+    redirect_to task_path(params[:id])
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_path
+  end
+
   private
 
   def task_params
